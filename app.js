0318b60cca4078367e0f216b7a80c3a490a8856e
Caique Almeida <caaguima@gmail.com>
2022-11-14 16:55:51 -0300

    feat: only change operator

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 [] Mostrar os dois numeros e operadores no visor
   [] Só deixar colocar um numero, depois um operador, depois um numero 
+  [] O 0 nunca pode ser o primeiro numero
+  [] 
 
 [] Realizar a conta do visor e mostrar resultado
 */
@@ -13,7 +15,6 @@ const clear = document.querySelector("#clear-btn");
 const visor = document.querySelector("#viewfinder");
 
 let mode = "number";
-// 'operator'
 
 function verifyWhatWasClicked(event) {
   switch (event) {
@@ -39,28 +40,42 @@ function verifyWhatWasClicked(event) {
   }
 }
 
-function changeViewFinder(value) {
-  if (mode === "number") {
+function changeViewFinder(value, type) {
+  if (mode === "operator" && type === "operator") {
+    let actualViewfinderText = visor.innerHTML;
+
+    const newViewfinderText = `${actualViewfinderText.substring(
+      0,
+      actualViewfinderText.length - 1
+    )}${value}`;
+
+    mode = "operator";
+    return (visor.innerHTML = newViewfinderText);
+  }
+
+  if (mode === "number" && type === "operator") {
+    mode = "operator";
     return (visor.innerHTML += value);
   }
 
-  if (mode === "operator") {
-    return (visor.innerHTML += value);
-  }
+  mode = "number";
+  return (visor.innerHTML += value);
 }
 
 function callbackListener(element) {
   element.addEventListener("click", () => {
-    let value;
+    let value, type;
     if (element.dataset.number) {
       value = element.dataset.number;
+      type = "number";
     }
 
     if (element.dataset.operator) {
       value = element.dataset.operator;
+      type = "operator";
     }
 
-    changeViewFinder(value);
+    changeViewFinder(value, type);
   });
 }
 
