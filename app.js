8db1cc6f9ec82dc59ef2d90bcefcd533c95bf5e2
Gabriel Antunes <113902295+gabrielpedoa@users.noreply.github.com>
2022-11-14 19:46:56 -0300

    Revert "feat: only change operator"

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 
 [] Mostrar os dois numeros e operadores no visor
   [] Só deixar colocar um numero, depois um operador, depois um numero 
-  [] O 0 nunca pode ser o primeiro numero
-  [] 
 
 [] Realizar a conta do visor e mostrar resultado
 */
@@ -15,6 +13,7 @@ const clear = document.querySelector("#clear-btn");
 const visor = document.querySelector("#viewfinder");
 
 let mode = "number";
+// 'operator'
 
 function verifyWhatWasClicked(event) {
   switch (event) {
@@ -40,42 +39,28 @@ function verifyWhatWasClicked(event) {
   }
 }
 
-function changeViewFinder(value, type) {
-  if (mode === "operator" && type === "operator") {
-    let actualViewfinderText = visor.innerHTML;
-
-    const newViewfinderText = `${actualViewfinderText.substring(
-      0,
-      actualViewfinderText.length - 1
-    )}${value}`;
-
-    mode = "operator";
-    return (visor.innerHTML = newViewfinderText);
-  }
-
-  if (mode === "number" && type === "operator") {
-    mode = "operator";
+function changeViewFinder(value) {
+  if (mode === "number") {
     return (visor.innerHTML += value);
   }
 
-  mode = "number";
-  return (visor.innerHTML += value);
+  if (mode === "operator") {
+    return (visor.innerHTML += value);
+  }
 }
 
 function callbackListener(element) {
   element.addEventListener("click", () => {
-    let value, type;
+    let value;
     if (element.dataset.number) {
       value = element.dataset.number;
-      type = "number";
     }
 
     if (element.dataset.operator) {
       value = element.dataset.operator;
-      type = "operator";
     }
 
-    changeViewFinder(value, type);
+    changeViewFinder(value);
   });
 }
 
